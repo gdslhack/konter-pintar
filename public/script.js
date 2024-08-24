a7abe6b5587ce2433495992e011a8a7298440fc7
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-08-24 16:30:55 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('/api/products')
         .then(response => response.json())
         .then(data => {
+            window.productData = data; // Simpan data produk di window untuk digunakan nanti
             populateProductOptions(data);
         })
         .catch(error => console.error('Error fetching products:', error));
@@ -51,12 +52,26 @@ function handleProductChange() {
     const product = document.getElementById('product').value;
     const plnOptions = document.getElementById('plnOptions');
     const phoneNumberInput = document.getElementById('phoneNumberInput');
+    const productData = window.productData[product];
 
     if (product === 'pln') {
+        populateDenomOptions(productData);
         plnOptions.style.display = 'block';
         phoneNumberInput.style.display = 'none';
     } else {
         plnOptions.style.display = 'none';
         phoneNumberInput.style.display = 'block';
     }
+}
+
+function populateDenomOptions(productData) {
+    const denomSelect = document.getElementById('denom');
+    denomSelect.innerHTML = ''; // Kosongkan opsi denom sebelumnya
+
+    productData.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item.denom; // Misalnya, ini adalah format denom
+        option.textContent = `${item.denom}k - Rp ${item.harga}`; // Tampilkan denom dan harga
+        denomSelect.appendChild(option);
+    });
 }
